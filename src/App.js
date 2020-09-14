d5c1caffbd6809701355dc68ac4c3c37c389fd82
nilslambertz <nilslambertz@web.de>
2020-09-14 23:18:02 +0200

    added settings-bar with sliders for elements, speed, creating new array and start/stop animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import NavBar from "./Components/Navigation/NavBar";
+import Main from "./Components/Main/Main";
 
 class App extends React.Component{
     state = {
-        algorithm: 0
+        algorithm: 0,
+        numberOfElements: 100,
+        animationSpeed: 500,
+        animationRunning: false
     }
 
     setAlgorithm = (nr) => {
@@ -13,14 +17,74 @@ class App extends React.Component{
         });
     }
 
+    changeSpeed = (e) => {
+        if(this.state.animationRunning) return;
+        this.setState({animationSpeed: e.target.value});
+    }
+
+    changeElemNumber = (event) => {
+        if(this.state.animationRunning) return;
+        Main.createNewArray();
+        this.setState({numberOfElements: event.target.value});
+    }
+
+    animationClick = () => {
+        let newState = !this.state.animationRunning;
+        this.setState({animationRunning: newState});
+    }
+
+    newArrayClick = () => {
+        if(this.state.animationRunning) return;
+        Main.createNewArray();
+    }
+
     render() {
         return (
             <div className="App">
-                <NavBar algorithm={this.state.algorithm} setAlgorithm={this.setAlgorithm}/>
+                <NavBar algorithm={this.state.algorithm} animationRunning={this.state.animationRunning} setAlgorithm={this.setAlgorithm}/>
+                <div id="settingsDiv">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td className={(this.state.animationRunning ? "disabled" : "")}>
+                                    <input type="range" min="5" max="600" value={this.state.numberOfElements} onChange={this.changeElemNumber}/>
+                                </td>
+                                <td className="settingsButton" rowSpan="2" style={this.state.animationRunning ? stopStyle : playStyle} onClick={this.animationClick}>
+                                    {this.state.animationRunning ? "stop" : "start"}
+                                </td>
+                                <td className={"settingsButton" + (this.state.animationRunning ? " disabled" : "")} rowSpan="2" style={newArrayStyle} onClick={this.newArrayClick}>
+                                    new array
+                                </td>
+                                <td className={(this.state.animationRunning ? "disabled" : "")}>
+                                    <input type="range" min="10" max="500" style={{direction: "ltr"}} value={this.state.animationSpeed} onChange={this.changeSpeed}/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className={(this.state.animationRunning ? "disabled" : "")}>
+                                    {this.state.numberOfElements} elements
+                                </td>
+                                <td className={(this.state.animationRunning ? "disabled" : "")}>
+                                    animation speed
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <Main algorithm={this.state.algorithm} numberOfElements={this.state.numberOfElements}/>
             </div>
         )
     }
+}
 
+const playStyle = {
+    color: "lightgreen"
+}
+const stopStyle = {
+    color: "orange"
+}
+
+const newArrayStyle = {
+    color: "lightblue"
 }
 
 export default App;
