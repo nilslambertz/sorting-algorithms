8284e88b09c7e7f7dc54a093b8d70d59c548ded9
nilslambertz <nilslambertz@web.de>
2020-09-19 02:11:20 +0200

    temporarily disabled some features

--- src/Algorithms/Bubblesort.js
+++ src/Algorithms/Bubblesort.js
@@ -1,3 +1,5 @@
+//import { endAnimation } from "../Utils/Animation";
+
 let swap = [];
 let array = [];
 
@@ -6,24 +8,49 @@ function bubbleSort() {
         for (let i = 0; i < n - 1; i++) {
             let a = {
                 firstIndex: i,
-                swapElements: false
+                elementsSwapped: false
             }
+            swap.push(a);
             if (array[i] > array[i + 1]) {
+                a = {
+                    firstIndex: i,
+                    elementsSwapped: true
+                }
                 let temp = array[i];
                 array[i] = array[i + 1];
                 array[i + 1] = temp;
-                a.swapElements = true;
+                swap.push(a);
             }
-            swap.push(a);
         }
     }
 }
 
-function doBubbleSort(arr) {
+function getBubbleSortSwap(arr) {
     swap = [];
     array = arr;
     bubbleSort();
     return swap;
 }
 
-export { doBubbleSort };
+function animateBubbleSort(array, swap, speed, interval) {
+    interval = setInterval(() => {
+        if(swap.length === 0) {
+         //   endAnimation(true);
+            return;
+        }
+        let x = swap.shift();
+        let first = x.firstIndex;
+
+        if(x.elementsSwapped === true) {
+            let array = [...this.state.array];
+            let temp = array[first];
+            array[first] = array[first+1];
+            array[first+1] = temp;
+            this.setState({array, firstIndex: first+1, secondIndex: first});
+        } else {
+            this.setState({firstIndex: first, secondIndex: first+1});
+        }
+    }, speed);
+}
+
+export { getBubbleSortSwap, animateBubbleSort };
