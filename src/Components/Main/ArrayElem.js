c57741ac1b6fa7bcf6a567777750449440b8e707
nilslambertz <nilslambertz@web.de>
2020-09-19 02:12:14 +0200

    highlights are now rendered in different colors, as well as the sorted array is rendered in green now

--- src/Components/Main/ArrayElem.js
+++ src/Components/Main/ArrayElem.js
@@ -3,8 +3,14 @@ import "./ArrayElem.css";
 
 class ArrayElem extends React.Component {
     render() {
+        let className = "arrayElem";
+        if(this.props.sorted) {
+            className += " sorted";
+        } else {
+            if(this.props.special) className += " " + this.props.special;
+        }
         return (
-            <div className="arrayElem" style={this.props.style}/>
+            <div className={className} style={this.props.style}/>
         )
     }
 }
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -4,8 +4,10 @@ import ArrayElem from "./ArrayElem";
 
 class Main extends React.Component {
     render() {
+        let className = "";
+        if(this.props.sorted === true) className = "sorted";
         return (
-            <div id="mainDiv">
+            <div id="mainDiv" className={className}>
                 {this.printArray()}
             </div>
         )
@@ -16,6 +18,8 @@ class Main extends React.Component {
         let len = arr.length;
         let width = Math.round(1000 / len);
         let margin = Math.floor((600 - len) / 300);
+        let first = this.props.firstIndex;
+        let second = this.props.secondIndex;
 
         let elemStyle = {
             width: width,
@@ -23,7 +27,10 @@ class Main extends React.Component {
         }
 
         return this.props.array.map(function(c,i,a) {
-            return <ArrayElem style={{...elemStyle, height: c + "px"}} key={i} array={a} index={i}/>;
+            let special = null;
+            if(i === first) special = "firstHighlight";
+            if(i === second) special = "secondHighlight";
+            return <ArrayElem style={{...elemStyle, height: c + "px"}} key={i} array={a} index={i} special={special}/>;
         })
     }
 }
