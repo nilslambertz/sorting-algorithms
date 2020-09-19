c5c2e2e6da3ba8aa88b859fe6a56490c5f64a4e4
nilslambertz <nilslambertz@web.de>
2020-09-19 22:46:54 +0200

    improved functions to animate using only one function and passing the needed algorithm-step-function to it

--- src/Utils/Animation.js
+++ src/Utils/Animation.js
@@ -1,5 +1,6 @@
 import { getBubbleSortSwap } from "../Algorithms/Bubblesort";
 import { arrayIsSorted } from "./Functions";
+import {getInsertionSortSwap} from "../Algorithms/InsertionSort";
 
 class Animation {
     setState;
@@ -36,7 +37,14 @@ class Animation {
                 if(this.swap.length === 0) {
                     this.swap = getBubbleSortSwap(this.array.slice(0));
                 }
-                this.animateBubbleSort();
+                this.animate(this.bubbleSortStep);
+                return true;
+            }
+            case 1: {
+                if(this.swap.length === 0) {
+                    this.swap = getInsertionSortSwap(this.array.slice(0));
+                }
+                this.animate(this.insertionSortStep);
                 return true;
             }
             default: {
@@ -57,24 +65,45 @@ class Animation {
         }
     }
 
-    animateBubbleSort() {
+    animate(step) {
         this.interval = setInterval(() => {
             if(this.swap.length === 0) {
                 this.endAnimation(true);
                 return;
             }
-            let x = this.swap.shift();
-            let first = x.firstIndex;
-
-            if(x.elementsSwapped === true) {
-                let temp = this.array[first];
-                this.array[first] = this.array[first+1];
-                this.array[first+1] = temp;
-                this.setState({array: this.array, firstIndex: first, secondIndex: first+1});
-            } else {
-                this.setState({firstIndex: first+1, secondIndex: first});
-            }
+            step();
         }, this.speed);
+    }
+
+    bubbleSortStep = () => {
+        let x = this.swap.shift();
+        let first = x.firstIndex;
+
+        if(x.elementsSwapped === true) {
+            let temp = this.array[first];
+            this.array[first] = this.array[first+1];
+            this.array[first+1] = temp;
+            this.setState({array: this.array, firstIndex: first, secondIndex: first+1});
+        } else {
+            this.setState({firstIndex: first+1, secondIndex: first});
+        }
+    }
+
+    insertionSortStep = () => {
+        let x = this.swap.shift();
+        let firstIndex = x.firstIndex;
+        let correctPosition = x.correctPosition;
+
+        if(correctPosition !== undefined) {
+            let temp = this.array[firstIndex];
+            for(let j = firstIndex; j > correctPosition; j--) {
+                this.array[j] = this.array[j-1];
+            }
+            this.array[correctPosition] = temp;
+            this.setState({array: this.array, firstIndex: null, secondIndex: correctPosition});
+        } else {
+            this.setState({firstIndex: firstIndex, secondIndex: null});
+        }
     }
 }
 
