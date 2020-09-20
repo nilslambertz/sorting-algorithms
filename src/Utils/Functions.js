ed4aeff4387fe16313894c58d4c3f9c5f17d640e
nilslambertz <nilslambertz@web.de>
2020-09-20 03:26:35 +0200

    added swap-function

--- src/Utils/Functions.js
+++ src/Utils/Functions.js
@@ -13,3 +13,9 @@ export const createNewArray = (elems, max) => {
     }
     return array;
 }
+
+export const swap = (arr, index1, index2) => {
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
