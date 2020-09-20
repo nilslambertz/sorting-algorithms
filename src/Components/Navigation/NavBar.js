b5ad00c2efa4616af4ecf2bcf260723924bb512e
nilslambertz <nilslambertz@web.de>
2020-09-20 02:43:40 +0200

    sorted nav-bar in alphabetical order

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -6,10 +6,10 @@ function NavBar({algorithm, setAlgorithm, animationRunning}) {
     return (
         <div id="navbar">
             <NavElem title="bubblesort" algoNumber="0" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
+            <NavElem title="cocktailshakersort" algoNumber="4" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
             <NavElem title="insertionsort" algoNumber="1" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
             <NavElem title="mergesort" algoNumber="2" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
             <NavElem title="quicksort" algoNumber="3" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
-            <NavElem title="cocktailshakersort" algoNumber="4" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
             <NavElem title="shellsort" algoNumber="5" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
         </div>
     );
