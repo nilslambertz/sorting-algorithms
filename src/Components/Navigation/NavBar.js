8937b52ab6d3af59cc709022a6ea6f8f1dfeda57
nilslambertz <nilslambertz@web.de>
2021-01-05 00:54:30 +0100

    added array with all algorithms, rendering of NavElems is now done in NavBar using divs, because own component for NavElem is unnecessary

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import './NavBar.css';
-import NavElem from './NavElem';
+
+const algorithms = ["bubblesort", "cocktailshakersort", "combsort", "insertionsort", "mergesort", "quicksort", "shellsort"]
 
 function NavBar({algorithm, setAlgorithm, animationRunning}) {
+    function changeAlgorithm(title) {
+        if(animationRunning === true) return;
+
+        setAlgorithm(title);
+    }
+
     return (
         <div id="navBar">
-            <NavElem title="bubblesort" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
-            <NavElem title="cocktailshakersort" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
-            <NavElem title="combsort" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
-            <NavElem title="insertionsort" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
-            <NavElem title="mergesort" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
-            <NavElem title="quicksort" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
-            <NavElem title="shellsort" setAlgorithm={setAlgorithm} currentAlgorithm={algorithm} animationRunning={animationRunning}/>
+            {algorithms.map(function(c,i,a) {
+                let className = "navElem";
+                if(c === algorithm) {
+                    className += " active";
+                } else if(animationRunning){
+                    className += " disabled"
+                }
+                return <div className={className} onClick={() => changeAlgorithm(c)}>{c}</div>;
+            })}
         </div>
     );
 }
