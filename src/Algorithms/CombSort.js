afeed327a87f9ed3481973175bb85e25de1b8d92
nilslambertz <nilslambertz@web.de>
2021-01-04 20:36:54 +0100

    implemented combSort

---
+++ src/Algorithms/CombSort.js
@@ -0,0 +1,47 @@
+let swap;
+let array;
+
+function combSort() {
+    let gap = array.length;
+    let shrink = 1.3;
+    let sorted = false;
+
+    while(!sorted) {
+        gap = Math.floor(gap / shrink);
+        if(gap < 1) {
+            gap = 1;
+            sorted = true;
+        }
+
+        for (let i = 0; i + gap < array.length; i++) {
+            let a = {
+                firstIndex: i,
+                secondIndex: i+gap,
+                elementsSwapped: false
+            }
+            swap.push(a);
+            if(array[i] > array[i+gap]) {
+                let temp = array[i];
+                array[i] = array[i+gap];
+                array[i+gap] = temp;
+                let a = {
+                    firstIndex: i,
+                    secondIndex: i+gap,
+                    elementsSwapped: true
+                }
+                swap.push(a);
+            }
+        }
+    }
+}
+
+function getCombSortSwap(arr) {
+    swap = [];
+    array = arr;
+    console.log(arr.slice(0));
+    combSort();
+    console.log(arr);
+    return swap;
+}
+
+export { getCombSortSwap };
