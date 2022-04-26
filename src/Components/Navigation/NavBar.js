c5d268866108397fef0d0283b8dd2d8b72f2f2b7
nilslambertz <nilslambertz@web.de>
2022-04-27 00:42:30 +0200

    update react, start implementing tailwind

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import './NavBar.css';
+import React from "react";
 
-function NavBar({algorithms, currentAlgorithm, setAlgorithm, animationRunning}) {
-    function changeAlgorithm(title) {
-        if(animationRunning === true) return;
+function NavBar({
+  algorithms,
+  currentAlgorithm,
+  setAlgorithm,
+  animationRunning,
+}) {
+  function changeAlgorithm(title) {
+    if (animationRunning === true) return;
 
-        setAlgorithm(title);
-    }
+    setAlgorithm(title);
+  }
 
-    return (
-        <div id="navBar">
-            {algorithms.map(function(c,i,a) {
-                let className = "navElem";
-                if(c === currentAlgorithm) {
-                    className += " active";
-                } else if(animationRunning){
-                    className += " disabled"
-                }
-                return <div key={c} className={className} onClick={() => changeAlgorithm(c)}>{c}</div>;
-            })}
-        </div>
-    );
+  return (
+    <div
+      id="navBar"
+      className="w-full select-none h-20 border-b flex flex-row flex-wrap justify-center items-center gap-10"
+    >
+      {algorithms.map(function (c, i, a) {
+        let className = "";
+        if (c === currentAlgorithm) {
+          className = "!text-white underline";
+        } else if (animationRunning) {
+          className = "!cursor-default opacity-10 hover:scale-100";
+        }
+        return (
+          <div
+            key={c}
+            className={
+              "text-2xl text-gray-400 cursor-pointer hover:scale-105 transition-all " +
+              className
+            }
+            onClick={() => changeAlgorithm(c)}
+          >
+            {c}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default NavBar;
