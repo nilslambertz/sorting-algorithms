8be12f68531440c3fb4016cafc107b492d3179a9
nilslambertz <nilslambertz@web.de>
2022-04-28 00:55:40 +0200

    fix ArrayElem-styles

--- src/Components/Main/ArrayElem.tsx
+++ src/Components/Main/ArrayElem.tsx
@@ -1,18 +1,15 @@
 import React from "react";
-import "./ArrayElem.css";
 
 interface ArrayElemProps {
-  sorted?: boolean;
   special?: string;
-  style?: React.CSSProperties;
+  value?: number;
 }
 
-export default function ArrayElem({ sorted, special, style }: ArrayElemProps) {
-  let className = "arrayElem bg-gray-300 ";
-  if (sorted) {
-    className += " transition-colors !bg-green-600";
-  } else {
-    if (special) className += " " + special;
-  }
-  return <div className={"flex-1 min-w-[1px] " + className} style={style} />;
+export default function ArrayElem({ special, value }: ArrayElemProps) {
+  return (
+    <div
+      className={"flex-1 min-w-[1px] arrayElem bg-gray-300 " + special}
+      style={{ height: value + "px" }}
+    />
+  );
 }
--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -5,13 +5,14 @@ import ArrayElem from "./ArrayElem";
 interface MainProps {
   currentStep?: StepDetails;
   array?: number[];
+  sorted?: boolean;
 }
 
-export default function Main({ array, currentStep }: MainProps) {
+export default function Main({ array, currentStep, sorted }: MainProps) {
   const firstHighlight = currentStep?.firstHighlight;
   const secondHighlight = currentStep?.secondHighlight;
-  const firstArea = currentStep?.firstArea;
-  const secondArea = currentStep?.secondArea;
+  const firstArea = currentStep?.firstArea ?? [-1, -1];
+  const secondArea = currentStep?.secondArea ?? [-1, -1];
 
   return (
     <div
@@ -20,16 +21,17 @@ export default function Main({ array, currentStep }: MainProps) {
     >
       {array?.map((value, index) => {
         let specialStyle = "";
+        if (firstArea[0] <= index && index <= firstArea[1])
+          specialStyle = "bg-orange-500";
+        if (secondArea[0] <= index && index <= secondArea[1])
+          specialStyle = "bg-orange-600";
+
         if (index === firstHighlight) specialStyle = "bg-blue-500";
         if (index === secondHighlight) specialStyle = "bg-blue-400";
 
-        return (
-          <ArrayElem
-            key={index}
-            special={specialStyle}
-            style={{ height: value + "px" }}
-          />
-        );
+        if (sorted) specialStyle = "transition-colors !bg-green-600";
+
+        return <ArrayElem key={index} special={specialStyle} value={value} />;
       })}
     </div>
   );
