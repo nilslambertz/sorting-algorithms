1184e6644b627bf4b5b9415640aada039753ba12
nilslambertz <nilslambertz@web.de>
2022-04-27 21:43:42 +0200

    use TS for helper functions

---
+++ src/Utils/Functions.ts
@@ -0,0 +1,26 @@
+export const arrayIsSorted = (arr: number[]) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) return false;
+  }
+  return true;
+};
+
+export const createNewArray = (numberOfElems: number, maxValue: number) => {
+  let allValues = [...Array(maxValue).keys()];
+  let array = [];
+  for (let i = 0; i < numberOfElems; i++) {
+    array[i] =
+      allValues.splice(Math.floor(Math.random() * allValues.length), 1)[0] + 10;
+  }
+  return array;
+};
+
+export const swap = (
+  arr: number[],
+  firstIndex: number,
+  secondIndex: number
+) => {
+  let temp = arr[firstIndex];
+  arr[firstIndex] = arr[secondIndex];
+  arr[secondIndex] = temp;
+};
