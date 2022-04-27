2ac90e3700c8bee6cc7cfac1e412af0f3d01b3e8
nilslambertz <nilslambertz@web.de>
2022-04-27 21:44:12 +0200

    convert BubbleSort to TS

---
+++ src/Algorithms/BubbleSort.ts
@@ -0,0 +1,38 @@
+import { StepDetails } from "../Utils/Types";
+
+function bubbleSort(array: number[]): StepDetails[] {
+  const steps: StepDetails[] = [];
+
+  for (let n = array.length; n > 1; n--) {
+    for (let i = 0; i < n - 1; i++) {
+      const firstStep: StepDetails = {
+        firstHighlight: i,
+        secondHighlight: i + 1,
+      };
+      steps.push(firstStep);
+
+      if (array[i] > array[i + 1]) {
+        let temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+
+        const secondStep = {
+          swap: [[i, i + 1]],
+          firstHighlight: i + 1,
+          secondHighlight: i,
+        };
+        steps.push(secondStep);
+      }
+    }
+  }
+
+  return steps;
+}
+
+function getBubbleSortSteps(arr: number[]): StepDetails[] {
+  const array = [...arr];
+
+  return bubbleSort(array);
+}
+
+export { getBubbleSortSteps };
