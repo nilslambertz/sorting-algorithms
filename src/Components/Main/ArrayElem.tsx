96ce45909d09e0b124cea42e21824951c31adefc
nilslambertz <nilslambertz@web.de>
2022-04-27 18:47:03 +0200

    add TypeScript, refactor ArrayElem to TS

---
+++ src/Components/Main/ArrayElem.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ArrayElem.css";
+
+interface ArrayElemProps {
+  sorted?: boolean;
+  special?: string;
+  style?: React.CSSProperties;
+}
+
+export default function ArrayElem({ sorted, special, style }: ArrayElemProps) {
+  let className = "arrayElem bg-gray-300 ";
+  if (sorted) {
+    className += " transition-colors !bg-green-600";
+  } else {
+    if (special) className += " " + special;
+  }
+  return <div className={"flex-1 " + className} style={style} />;
+}
