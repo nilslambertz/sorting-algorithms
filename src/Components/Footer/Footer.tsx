c85f5db2d52b2ce29de5ab4aeb2045fd959645dc
nilslambertz <nilslambertz@web.de>
2022-05-04 00:49:56 +0200

    fix repo link

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
       </a>
       -
       <a
-        href="https://github.com/nilslambertz/ArraySortReact"
+        href="https://github.com/nilslambertz/sorting-algorithms"
         target="_blank"
         rel="noopener noreferrer"
         className="transition-colors hover:text-white"
